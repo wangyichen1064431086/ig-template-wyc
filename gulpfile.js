7885992bc6d3da78c42413cd8b137c340246f297
wangyichen1064431086 <1064431086@qq.com>
2016-10-24 23:15:20 +0800

    1024 homework

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ const argv = minimist(process.argv.slice(2),knownOptions);
 */
 const contentDataFile = path.resolve(__dirname,'data',argv.i+'.json');//设置内容数据文件路径：__dirname/data/myanmar.json
 const footerDataFile = path.resolve(__dirname,'data','footer.json');//设置脚部数据文件路径：__dirname/data/footer.json
-
+const projectName = 
 
 
 gulp.task('prod',function(done){
@@ -205,13 +205,14 @@ gulp.task('serve',gulp.paraller('mustache','styles','webpack',function serve(){
 }));
 
 /***build***/
+///任务rollup:将client/js/main.js编译、压缩，将结果保存到.tmp/script/main.js
 //use rollup and buble to build js
 gulp.task('rollup',() => {
 	return rollup({//新一代ES6的模块打包机
 		entry:'client/js/main.js',
 		plugins:[
 			bowerResolve(),//在你的bower component目录下，通过（bower使用的针对第三方模块的解析算法）来定位模块
-			buble(),//速度极快的、电池类的ES2015编译器
+			buble(),//速度极快的、电池类的ES2015编译器(疑问：是要将ES6编译为ES5吗？)
 			uglify()////卷曲相关插件以压缩最后生成的包
 		],
 		cache:cache//前面定义了var cache,故此处catch为null。
@@ -230,11 +231,46 @@ gulp.task('rollup',() => {
 	});
 });
 
-
+///任务custom:将custom/目录下的projectName.js/projectName.css拷贝到.tmp/目录下
 gulp.task('custom',()=>{
-	const SRC = 'custom/**/' + projectName +'.{js,css}';//projectName并未定义，这个任务是没用的吧？？
+	const SRC = 'custom/**/' + projectName +'.{js,css}';
 	const DEST = '.tmp';
 	console.log('Copy custom js and css files to:',DEST);
 	return gulp.src(SRC)
 		.pipe(gulp.dest(DEST));
-})
+});
+
+
+///任务prefix：为.tmp/index.html中的$('picture source')元素的srcset属性值（是一个由逗号分隔的字符串）分别添加前缀
+gulp.task('prefix',() => {
+	return gulp.src('.tmp/index.html')
+		.pipe($.cheerio(function($,file){
+			/*
+			 * cheerio:为服务器特别定制的小型、快速、优雅的jQuery核心实现。
+			 * gulp-cheerio是gulp的cheerio
+			 * 此处用的是gulp-cheerio的同步方法
+			*/
+			$('picture source').each(function(){//为每一个$('picture source')执行如下函数
+				var source = $(this);
+				var srcset = source.attr('srcset');//获取$('picture source')的属性'srcset'
+				if (srcset) {
+					srcset = srcset.split(',').map(
+						/* split:把一个String分割成一个字符串数组。
+						 * map(callback):返回一个由原数组中的每个元素调用一个指定方法后的返回值组成的新数组。
+						 ** map的callback的参数：
+						 ** - currentValue(第一个参数：数组中当前被传递的元素)
+						 ** - index(第二个参数：数组中当前被传递的元素的索引)
+						 ** - array(第三个参数：调用map方法的数组)
+						*/
+						function(href){//此处形参href即为数组中当前被传递的元素
+						return url.resolve(config.imgPrefix,href).replace('%20',' ');
+						/*config.imgPrefix:"http://interactive.ftchinese.com/"
+						 *该句话就是将（"http://interactive.ftchinese.com/"） 和 （$('picture source')的属性srcset的值中逗号分隔的每个值）分别拼接起来，并将拼接结果中的'%20'替换为' '
+						 */
+					}).join(', ');//然后再将加上了"http://interactive.ftchinese.com/"前缀的数组join成逗号分隔的字符串
+					source.attr('srcset',srcset);//用新的srcset为$('picture source')的属性'srcset'赋值。
+				}
+			});
+		}))
+		.pipe(gulp.dest('dist'));
+});
